b1c984166228c47bd4d1b01fed2ac30d2ca3fcba
usha-v-12 <ushav122006@gmail.com>
2025-09-04 22:33:52 +0530

    Update Simple Calculator.py

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -7,7 +7,7 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
 # Choose the operation
-print("\nSelect operation:")
+print("Select operation:")
 print("1. Add")
 print("2. Subtract")
 print("3. Multiply")
@@ -33,3 +33,4 @@ elif operation == "4":
         print("Error: Division by zero is not allowed.")
 else:
     print("Invalid operation choice")
+
